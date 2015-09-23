c3f7e50a1495aeb70cf9d5af9e14c2bf5f258b41
Chandan Jyoti Sharma <chandan.jyoti.sharma@intel.com>
2015-09-23 16:09:50 -0700

    fixed basic layout

--- RouteCar/app/src/main/java/com/routecar/MapActivity.java
+++ RouteCar/app/src/main/java/com/routecar/MapActivity.java
@@ -1,17 +1,22 @@
 package com.routecar;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MapActivity extends ActionBarActivity {
+public class MapActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view =  inflater.inflate(R.layout.layout1,null);
+        setContentView(view);
     }
 
 
