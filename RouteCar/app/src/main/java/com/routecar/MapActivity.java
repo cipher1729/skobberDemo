b553f339370080116d07f27c178e146b6a5cc162
Chandan Jyoti Sharma <chandan.jyoti.sharma@intel.com>
2015-09-24 15:23:03 -0700

    displays basic map with two text boxes

--- RouteCar/app/src/main/java/com/routecar/MapActivity.java
+++ RouteCar/app/src/main/java/com/routecar/MapActivity.java
@@ -8,17 +8,39 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.routecar.application.DemoApplication;
+import com.routecar.util.DemoUtils;
 import com.skobbler.ngx.SKMaps;
+import com.skobbler.ngx.map.SKAnnotation;
+import com.skobbler.ngx.map.SKCoordinateRegion;
+import com.skobbler.ngx.map.SKMapCustomPOI;
+import com.skobbler.ngx.map.SKMapPOI;
+import com.skobbler.ngx.map.SKMapSurfaceListener;
+import com.skobbler.ngx.map.SKMapSurfaceView;
+import com.skobbler.ngx.map.SKMapViewHolder;
+import com.skobbler.ngx.map.SKPOICluster;
+import com.skobbler.ngx.map.SKScreenPoint;
 
-public class MapActivity extends Activity {
+import java.nio.charset.MalformedInputException;
+
+public class MapActivity extends Activity implements SKMapSurfaceListener{
+//app local variables go here
+//
+    private SKMapSurfaceView mapView;
+    private SKMapViewHolder mapViewGroup;
+    DemoApplication app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SKMaps.getInstance().initializeSKMaps(this, null);
+        //SKMaps.getInstance().initializeSKMaps(this, null);
+        DemoUtils.initializeLibrary(MapActivity.this);
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view =  inflater.inflate(R.layout.layout1,null);
         setContentView(view);
+        app = (DemoApplication) getApplication();
+        mapViewGroup = (SKMapViewHolder) findViewById(R.id.view_group_map);
+        mapViewGroup.setMapSurfaceListener(MapActivity.this);
     }
 
 
@@ -38,5 +60,149 @@ public class MapActivity extends Activity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActionPan() {
+
+    }
+
+    @Override
+    public void onActionZoom() {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(SKMapViewHolder mapHolder) {
+        View chessBackground = findViewById(R.id.chess_board_background);
+        chessBackground.setVisibility(View.GONE);
+        mapView = mapHolder.getMapSurfaceView();
+        applySettingsOnMapView();
+    }
+
+    @Override
+    public void onMapRegionChanged(SKCoordinateRegion skCoordinateRegion) {
+
+    }
+
+    @Override
+    public void onMapRegionChangeStarted(SKCoordinateRegion skCoordinateRegion) {
+
+    }
+
+    @Override
+    public void onMapRegionChangeEnded(SKCoordinateRegion skCoordinateRegion) {
+
+    }
+
+    @Override
+    public void onDoubleTap(SKScreenPoint point) {
+        //mapView.zoomInAt(point);
+    }
+
+    @Override
+    public void onSingleTap(SKScreenPoint skScreenPoint) {
+       // mapPopup.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onRotateMap() {
+
+    }
+
+    @Override
+    public void onLongPress(SKScreenPoint skScreenPoint) {
+
+    }
+
+    @Override
+    public void onInternetConnectionNeeded() {
+
+    }
+
+    @Override
+    public void onMapActionDown(SKScreenPoint skScreenPoint) {
+
+    }
+
+    @Override
+    public void onMapActionUp(SKScreenPoint skScreenPoint) {
+
+    }
+
+    @Override
+    public void onPOIClusterSelected(SKPOICluster skpoiCluster) {
+
+    }
+
+    @Override
+    public void onMapPOISelected(SKMapPOI skMapPOI) {
+
+    }
+
+    @Override
+    public void onAnnotationSelected(SKAnnotation skAnnotation) {
+
+    }
+
+    @Override
+    public void onCustomPOISelected(SKMapCustomPOI skMapCustomPOI) {
+
+    }
+
+    @Override
+    public void onCompassSelected() {
+
+    }
+
+    @Override
+    public void onCurrentPositionSelected() {
+
+    }
+
+    @Override
+    public void onObjectSelected(int i) {
+
+    }
+
+    @Override
+    public void onInternationalisationCalled(int i) {
+
+    }
+
+    @Override
+    public void onBoundingBoxImageRendered(int i) {
+
+    }
+
+    @Override
+    public void onGLInitializationError(String s) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapViewGroup.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapViewGroup.onResume();
+    }
+
+
+    /**
+     * Customize the map view
+     */
+    private void applySettingsOnMapView() {
+        mapView.getMapSettings().setMapRotationEnabled(true);
+        mapView.getMapSettings().setMapZoomingEnabled(true);
+        mapView.getMapSettings().setMapPanningEnabled(true);
+        mapView.getMapSettings().setZoomWithAnchorEnabled(true);
+        mapView.getMapSettings().setInertiaRotatingEnabled(true);
+        mapView.getMapSettings().setInertiaZoomingEnabled(true);
+        mapView.getMapSettings().setInertiaPanningEnabled(true);
     }
 }
